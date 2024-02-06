7a544756d889b29d21b0312a51a3444a85de4c0d
Paulo Aguiar <aguiarlapaulo@gmail.com>
2024-02-06 11:12:38 -0300

    feat: Adding tests

---
+++ src/contract.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel
+from datetime import datetime
+
+class Sales(BaseModel):
+    "email": str
+    "date": datetime
+    "value": int
+    "product": str
+    "quantity": int
+    "category": str
---
+++ tests/tests_contract.py
@@ -0,0 +1,54 @@
+import pytest
+from datetime import datetime
+from src.contract import Sales
+from pydantic import ValidationError
+
+def test_sales_data_check():
+    """
+    This test verifies that the Sales class correctly accepts and stores the valid data provided.
+    Valid data includes a correct email, current date, positive value, product name, positive quantity
+    and an existing category. The test confirms that the values stored in the instance match the data provided.
+    """
+    data_check = {
+                "email": "comprador@example.com",
+                "date": datetime.now(),
+                "value": 100.50,
+                "product": "Product X",
+                "quantity": 3,
+                "category": "category3",
+            }
+
+    sales = Sales(**data_check)
+
+    assert sales.email == data_check["email"]
+    assert sales.date == data_check["date"]
+    assert sales.value == data_check["value"]
+    assert sales.product == data_check["product"]
+    assert sales.quantity == data_check["quantity"]
+    assert sales.category == data_check["category"]
+
+def test_sales_invalid_data_check():
+    invalid_data_check = {
+            "email": "comprador",
+            "date": "it is not date",
+            "value": -100,
+            "product": "",
+            "quantity": -1,
+            "category": "category3",
+        }
+    
+    with pytest.raises(ValidationError):
+        Sales(**invalid_data_check)
+
+def test_category_invalid_data_check():
+    category_invalid_data_check = {
+            "email": "comprador@example.com",
+            "date": datetime.now(),
+            "value": 100.50,
+            "product": "Product X",
+            "quantity": 3,
+            "category": "not included",
+        }
+    
+    with pytest.raises(ValidationError):
+        Sales(**category_invalid_data_check)
