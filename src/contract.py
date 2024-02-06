a1599a734f27df4989b04a9c75836ddef03db08d
Paulo Aguiar <aguiarlapaulo@gmail.com>
2024-02-06 11:17:26 -0300

    feat:Fixing test file name

--- src/contract.py
+++ src/contract.py
@@ -2,9 +2,9 @@ from pydantic import BaseModel
 from datetime import datetime
 
 class Sales(BaseModel):
-    "email": str
-    "date": datetime
-    "value": int
-    "product": str
-    "quantity": int
-    "category": str
+    email: str
+    date: datetime
+    value: int
+    product: str
+    quantity: int
+    category: str
