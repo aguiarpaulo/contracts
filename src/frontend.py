08043898b1e38c88acbb9e4983de04e7ce41d1f7
Paulo Aguiar <aguiarlapaulo@gmail.com>
2024-02-07 15:57:08 -0300

    feat: Add web test

---
+++ src/frontend.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+class ExcelValidatorUI:
+    def __init__(self):
+        self.set_page_config()
+
+    def set_page_config(self):
+        st.set_page_config(
+            page_title='Excel Schema Validator'
+        )
+
+    def display_header(self):
+        st.title("Attach the excel file")
+
+    def upload_file(self):
+        return st.file_uploader("Upload the excel file here", type=['xlsx'])
