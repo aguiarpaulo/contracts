af123b14f1b2b0112a68d433d7bbf27159f8027e
Paulo Aguiar <aguiarlapaulo@gmail.com>
2024-02-10 12:05:03 -0300

    feat: adding backend.py

--- src/backend.py
+++ src/backend.py
@@ -14,25 +14,29 @@ POSTGRES_DB = os.getenv("POSTGRES_DB")
 DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
 
 def process_excel(uploaded_file):
+    df = None
+    error_message = None
+    
     try:
         df = pd.read_excel(uploaded_file)
 
-        extra_cols = set(df.colunms) - set(Sales.model_fields.keys())
+        extra_cols = set(df.columns) - set(Sales.model_fields.keys())
         if extra_cols:
-            return False, f"Extra Colunms found: {', '.join(extra_cols)}"
-    
-        for index, row in df.iterrows():
-            try:
-                _=Sales(**row.to_dict())
-            except Exception as e:
-                raise ValueError(f"Error row {index + 2}: {e}")
-
-        return True, None
+            error_message = f"Extra Columns found: {', '.join(extra_cols)}"
+        else:
+            for index, row in df.iterrows():
+                try:
+                    _ = Sales(**row.to_dict())
+                except Exception as e:
+                    raise ValueError(f"Error row {index + 2}: {e}")
 
     except ValueError as ve:
-        return False #,str(ve)
+        error_message = str(ve)
     except Exception as e:
-        return False, f"Unexpected error: {str(e)}"
+        error_message = f"Unexpected error: {str(e)}"
+
+    success = error_message is None
+    return df, error_message
 
 def excel_to_sql(df):
-    df.to_sql('sales', con=DATABASE_URL, if_exists='replace', index=False)    
+    df.to_sql('sales', con=DATABASE_URL, if_exists='replace', index=False)
